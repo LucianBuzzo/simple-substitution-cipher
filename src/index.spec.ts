11b3525fb9e31333764ee3a18756fcd11ccbe4e9
Lucian Buzzo <lucian.buzzo@gmail.com>
2023-04-01 22:13:35 +0100

    crack with quadgrams!

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,16 +1,18 @@
-import { decipher, encipher, generateCipherAlphabetFromKey } from "./index";
+import { crack, decipher, encipher, generateCipherAlphabetFromKey } from "./index";
+
+// Sample text from https://en.wikipedia.org/wiki/The_Matrix
+const SAMPLE_TEXT = `
+The Matrix is a 1999 science fiction action film written and directed by The Wachowskis, starring Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving, and Joe Pantoliano. It depicts a dystopian future in which reality as perceived by most humans is actually a simulated reality called "the Matrix", created by sentient machines to subdue the human population, while their bodies' heat and electrical activity are used as an energy source. Computer programmer Neo learns this truth and is drawn into a rebellion against the machines, which involves other people who have been freed from the "dream world".
+The Matrix is an example of the cyberpunk subgenre of science fiction.[8] The Wachowskis' approach to action scenes was influenced by Japanese animation[9] and martial arts films, and the film's use of fight choreographers and wire fu techniques from Hong Kong action cinema influenced subsequent Hollywood action film productions. The film popularized a visual effect known as "bullet time", in which the heightened perception of certain characters is represented by allowing the action within a shot to progress in slow-motion while the camera appears to move through the scene at normal speed, allowing the sped-up movements of certain characters to be perceived normally.
+The Matrix opened in theaters in the United States on March 31, 1999, to widespread acclaim from critics, who praised its innovative visual effects, action sequences, cinematography and entertainment value,[10][11] and was a massive success at the box office, grossing over $460 million on a $63 million budget, becoming the highest-grossing Warner Bros. film of 1999 and the fourth highest-grossing film of that year. At the 72nd Academy Awards, the film won all four categories it was nominated for, Best Visual Effects, Best Film Editing, Best Sound, and Best Sound Editing. The film was also the recipient of numerous other accolades, including Best Sound and Best Special Visual Effects at the 53rd British Academy Film Awards, and the Wachowskis were awarded Best Director and Best Science Fiction Film at the 26th Saturn Awards. The film is considered to be among the greatest science fiction films of all time,[12][13][14] and in 2012, the film was selected for preservation in the United States National Film Registry by the Library of Congress for being "culturally, historically, and aesthetically significant."[15]
+The film's success led to two feature film sequels being released in 2003, The Matrix Reloaded and The Matrix Revolutions, which were also written and directed by the Wachowskis. The Matrix franchise was further expanded through the production of comic books, video games and an animated anthology film, The Animatrix, with which the Wachowskis were heavily involved. The franchise has also inspired books and theories expanding on some of the religious and philosophical ideas alluded to in the films. A fourth film, titled The Matrix Resurrections, was released on December 22, 2021.
+`
 
 describe("Simple Substitution Cipher", () => {
   it("should encode a string", () => {
     const key = "aardvark";
     expect(encipher("hello", key)).toBe("nbpph");
     expect(encipher("peanut", key)).toBe("xbwilo");
-    console.log(
-      encipher(
-        "Some years later, Matrix is informed by his former superior, General Franklin Kirby, that members of his unit were killed by mercenaries hired by a ruthless warlord. Matrix shrugs this off but he is soon attacked by the same group of mercenaries. During the assault, his daughter is kidnapped. He then goes off on his own to find his daughter and destroy the men who tried to kill him. He gets into a car wreck and is captured by the gang.",
-        key
-      )
-    );
   });
 
   it("should decode a string", () => {
@@ -29,5 +31,25 @@ describe("Simple Substitution Cipher", () => {
     const alphabet2 = generateCipherAlphabetFromKey(key);
     expect(alphabet1).toBe("swezhqguiaxnopdvrkfbcjmylt");
     expect(alphabet1).toBe(alphabet2);
+  });
+
+  it("should work with long strings of text", () => {
+    const key = "aardvark";
+    const cipherText = encipher(SAMPLE_TEXT, key);
+    expect(decipher(cipherText, key)).toBe(
+      SAMPLE_TEXT.toLowerCase().replace(/[^a-z]/g, '')
+    )
+  });
+
+  it("should crack a cipher", () => {
+    const key = "aardvark";
+    const cipherText = encipher(SAMPLE_TEXT, key);
+    const processedText = SAMPLE_TEXT.toLowerCase().replace(/[^a-z]/g, '')
+    const cracked = crack(cipherText);
+    console.log(cracked)
+    expect(cracked.length).toBe(processedText.length)
+    expect(cracked).toBe(
+      processedText
+    )
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,37 @@
 import { MersenneTwister19937, createEntropy } from "random-js";
+import { readFileSync } from "fs";
 
-const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+const loadQuadgramData = (filename: string) => {
+  const fileContent = readFileSync(filename, "utf-8");
+  const lines = fileContent.split("\n");
+  const quadgramMap: Record<string, number> = {};
+
+  for (const line of lines) {
+    const [quadgram, frequency] = line.split(" ");
+    quadgramMap[quadgram.toLowerCase()] = parseInt(frequency, 10);
+  }
+
+  return quadgramMap;
+};
+
+const QUADGRAMS_FILE = "./english_quadgrams.txt";
+const QUADGRAMS = loadQuadgramData(QUADGRAMS_FILE);
+
+const quadgramScore = (text: string) => {
+  let score = 0;
+  const cleanedText = text.replace(/[^a-z]/g, "").toLowerCase();
+
+  for (let i = 0; i < cleanedText.length - 3; i++) {
+    const quadgram = cleanedText.slice(i, i + 4);
+    const quadgramFrequency = QUADGRAMS[quadgram] || 0;
+    score += Math.log(quadgramFrequency + 1);
+  }
+
+  return score;
+};
+
+export const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
 
 const stringTo32BitInteger = (str: string): number => {
   let hash = 0;
@@ -41,7 +72,7 @@ const createLookupTable = (sourceAlphabet: string, targetAlphabet: string) => {
     }, {});
 };
 
-const processText = (
+export const processText = (
   text: string,
   sourceAlphabet: string,
   targetAlphabet: string
@@ -49,6 +80,7 @@ const processText = (
   const lookupTable = createLookupTable(sourceAlphabet, targetAlphabet);
 
   return text
+    .toLowerCase()
     .split("")
     .map((char) => lookupTable[char])
     .join("");
@@ -62,4 +94,72 @@ export const encipher = (text: string, key: string) => {
 export const decipher = (ciphertext: string, key: string) => {
   const cipherAlphabet = generateCipherAlphabetFromKey(key);
   return processText(ciphertext, cipherAlphabet, ALPHABET);
+};
+
+const frequencyAnalysis = (text: string) => {
+  const processedText = text.toLowerCase().replace(/[^a-z]/g, '')
+  const charFrequency: Record<string, number> = {}
+  for (let char of processedText) {
+    if (charFrequency[char]) {
+      charFrequency[char]++
+    } else {
+      charFrequency[char] = 1
+    }
+  }
+  const totalChars = processedText.length
+  for (let char in charFrequency) {
+    charFrequency[char] = charFrequency[char] / totalChars
+  }
+  return charFrequency
+}
+
+export const crack = (cipherText: string) => {
+  // Remove non-alphabetic characters and convert to lowercase
+  const cleanedCipherText = cipherText.toLowerCase().replace(/[^a-z]/g, "");
+
+  // Function to swap two characters in a string at positions i and j
+  const swapPositions = (str: string, i: number, j: number) => {
+    const chars = str.split("");
+    [chars[i], chars[j]] = [chars[j], chars[i]];
+    return chars.join("");
+  };
+
+  // Function to get a random integer between min and max (inclusive)
+  const getRandomInt = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  // Set the maximum number of iterations for the hill-climbing algorithm
+  const MAX_ITERATIONS = 10000;
+
+  // Generate a random initial alphabet as the starting point
+  let bestAlphabet = ALPHABET.split("").sort(() => Math.random() - 0.5).join("");
+
+  // Decrypt the text using the initial alphabet and calculate its quadgram score
+  let bestText = processText(cleanedCipherText, ALPHABET, bestAlphabet);
+  let bestScore = quadgramScore(bestText);
+
+  // Iterate through the hill-climbing algorithm
+  for (let iteration = 0; iteration < MAX_ITERATIONS; iteration++) {
+    // Get two random positions in the alphabet
+    const i = getRandomInt(0, 25);
+    const j = getRandomInt(0, 25);
+
+    // Swap characters at the random positions in the current best alphabet
+    const newAlphabet = swapPositions(bestAlphabet, i, j);
+
+    // Decrypt the text using the new alphabet and calculate its quadgram score
+    const newText = processText(cleanedCipherText, ALPHABET, newAlphabet);
+    const newScore = quadgramScore(newText);
+
+    // If the new quadgram score is better, update the best alphabet, text, and score
+    if (newScore > bestScore) {
+      bestAlphabet = newAlphabet;
+      bestText = newText;
+      bestScore = newScore;
+    }
+  }
+
+  // Decipher the original cipher text using the best alphabet found
+  return processText(cipherText, ALPHABET, bestAlphabet)
 };
